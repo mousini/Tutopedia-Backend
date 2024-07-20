7be5bd7fecea1ed0897f7271fbed276681eada00
Mousini Sarkar <160837080+mousini@users.noreply.github.com>
2024-07-21 01:46:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
-var express=require("express")
-var bodyParser=require("body-parser")
-var mongoose=require("mongoose")
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-const app=express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-    extended:true
-}))
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/Database')
-var db=mongoose.connection
-db.on('error',()=> console.log("Error in Connecting to Database"))
-db.once('open',()=> console.log("Connected to Database"))
+mongoose.connect('mongodb://localhost:27017/Database', { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
 
-app.post("/sign_up",(req,res) => {
-    var name= req.body.name
-    var email=req.body.email
-    var password=req.body.password
+db.on('error', () => console.log("Error in Connecting to Database"));
+db.once('open', () => console.log("Connected to Database"));
 
-    var data={
-        "name":name,
-        "email":email,
-        "password":password
-    }
-    db.collection('tutopedia2').insertOne(data,(err,collection) => {
-        if(err){
+app.post("/sign_up", (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "password": password
+    };
+
+    db.collection('tutopedia2').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
-        console.log("Record Inserted Succesfully")
-    })
-    return res.redirect('signup_successful.html')
-})
+        console.log("Record Inserted Successfully");
+    });
 
-app.get("/",(req,res) => {
+    return res.redirect('signup_successful.html');
+});
+
+app.post("/sign_in", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    db.collection('tutopedia2').findOne({ email: email }, (err, user) => {
+        if (err) {
+            res.status(500).send('Error occurred while signing in');
+        } else if (!user) {
+            res.status(400).send('User not found');
+        } else if (user.password !== password) {
+            res.status(400).send('Invalid password');
+        } else {
+            res.send('Sign-in successful');
+        }
+    });
+});
+
+app.get("/", (req, res) => {
     res.set({
-        "Allow-acces-Allow-Origin":'*'
-    })
-    return res.redirect('index.html')
+        "Allow-access-Allow-Origin": '*'
+    });
+    return res.redirect('index.html');
 }).listen(3000);
 
-console.log("Listening on port 3000")
+console.log("Listening on port 3000");
